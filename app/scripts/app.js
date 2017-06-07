933e88961a32cfa1170522423e3c839f517353b9
Gleb Gorokhov <gg@verstaburo.com>
2017-06-07 15:09:37 +0300

    account full

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,9 +1,11 @@
 import $ from 'jquery';
 import { preloader } from '../blocks/preloader/preloader';
+import { tabs } from '../blocks/tabs/tabs';
 import slider from '../blocks/slider/slider';
 
 $(() => {
   // your code goes here
   slider();
   preloader();
+  tabs();
 });

8a2b57560feabafa0168cec0a99b3614f2cedb6d
Gleb Gorokhov <gg@verstaburo.com>
2017-06-08 16:45:15 +0300

    playlists final

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -2,12 +2,11 @@ import $ from 'jquery';
 import { preloader } from '../blocks/preloader/preloader';
 import { tabs } from '../blocks/tabs/tabs';
 import slider from '../blocks/slider/slider';
-import trackssize from '../blocks/right-content/tracks-size';
+//import trackssize from '../blocks/right-content/tracks-size';
 
 $(() => {
   // your code goes here
   slider();
   preloader();
   tabs();
-  //trackssize();
 });

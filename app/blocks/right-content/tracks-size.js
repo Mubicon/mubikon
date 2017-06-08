0e2d54a07a6caa40f840f632dad7cc2814d106fc
Gleb Gorokhov <gg@verstaburo.com>
2017-06-08 13:31:32 +0300

    podborki final

---
+++ app/blocks/right-content/tracks-size.js
@@ -0,0 +1,19 @@
+export default () => {
+  // eslint-disable-next-line no-unused-vars
+  function size() {
+    var wH = $(window).height(),
+        tracks = $(".right-tracks__content"),
+        offTop = tracks.offset().top;
+
+    var maxheight = wH - offTop - 30;
+    tracks.css("max-height", maxheight);
+  }
+
+  $(window).on("resize", function(){
+    size();
+  });
+
+  $(document).ready(function(){
+    size();
+  });
+};
--- app/scripts/app.js
+++ app/scripts/app.js
@@ -2,10 +2,12 @@ import $ from 'jquery';
 import { preloader } from '../blocks/preloader/preloader';
 import { tabs } from '../blocks/tabs/tabs';
 import slider from '../blocks/slider/slider';
+import trackssize from '../blocks/right-content/tracks-size';
 
 $(() => {
   // your code goes here
   slider();
   preloader();
   tabs();
+  //trackssize();
 });

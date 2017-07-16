fb85b7e900d467c9ece77ef827521e5e47937cda
Gleb Gorokhov <gg@verstaburo.com>
2017-07-16 18:24:08 +0300

    accordion fix

--- app/blocks/filter-container/filter-accordeon.js
+++ app/blocks/filter-container/filter-accordeon.js
@@ -1,23 +1,42 @@
 export default function () {
-  $(".filter-item, .feed-control__selectbox").click(function (e) {
+  // ##########################
+  // Открытие фильтра
+  // ##########################
+  $("[data-accordion-target]").click(function (e) {
+    var target = $(this).data('accordion-target');
+
     if ( $(this).hasClass('active') ) {
+
       e.preventDefault();
       $(this).removeClass('active').siblings().removeClass('active');
-      $('.filter-container__accordion-close:eq(0)')[0].click();
+      $(target).removeClass('active');
+
     } else {
+
       $(this).addClass('active').siblings().removeClass('active');
+      $(target).addClass('active').siblings().removeClass('active');
+
     }
   });
 
-  $(".filter-container__accordion-close").click(function(){
-    $(".filter-item, .feed-control__selectbox").removeClass('active');
+  // ##########################
+  // Закрытие фильтра
+  // ##########################
+  $("[data-accordion-action]").click(function(){
+    if ( $(this).data('accordion-action') === 'close' ) {
+      $("[data-accordion-target], .feed-control__selectbox").removeClass('active');
+      $(this).parents('.filter-container__accordion').removeClass('active');
+    }
   });
 
+  // ##########################
+  // Закрытие выпадающего списка
+  // ##########################
   $(".feed-control .filter-label").click(function(e){
     e.preventDefault();
     var value = $(this).text(),
         parent = $(this).parents('.filter-container__accordion').attr('id');
-    $('[href="#' + parent + '"]').find('.selectbox__select').text(value);
+    $('[data-accordion-target="#' + parent + '"]').find('.selectbox__select').text(value);
     $('.filter-container__accordion-close:eq(0)')[0].click();
   });
 }
